242aaad217ffd258b10ef64f6f656543f321d4ac
r.kazakov <kazakov302215@gmail.com>
2021-10-10 22:21:50 +0300

    added main methods, clickhandler() in progress

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
 import { Select } from './select/select'
 import './select/styles.scss'
 
-const select = new Select('#select', {})
+const select = new Select('#select', {
+  placeholder: 'Please choose fruit',
+  data: [
+    { id: '1', value: 'Orange' },
+    { id: '2', value: 'Apple' },
+    { id: '3', value: 'Pineapple' },
+    { id: '4', value: 'Passion fruit' },
+    { id: '5', value: 'Ambrosia' },
+    { id: '6', value: 'Kiwi' },
+    { id: '7', value: 'Melon' },
+    { id: '8', value: 'Pear' }
+  ]
+})
 
 window.selectPlugin = select
--- select/select.js
+++ select/select.js
@@ -1,31 +1,36 @@
-const getTemplate = () => `
-  <div class="select__input" data-type="input-field">
-    <span>Text</span>
-    <i class="fa fa-chevron-down" aria-hidden="true" data-type="arrow"></i>
-  </div>
-  <div class="select__dropdown">
-    <ul class="select__list">
-      <li class="select__item">1</li>
-      <li class="select__item">2</li>
-      <li class="select__item">3</li>
-      <li class="select__item">4</li>
-      <li class="select__item">5</li>
-      <li class="select__item">6</li>
-    </ul>
-  </div>
-`
+const getTemplate = (placeholder, data = []) => {
+  const inputText = placeholder ?? 'default placeholder'
+
+  const selectItems = data.map(
+    item => `<li class='select__item' data-type="select-item" data-value="${item.id}">${item.value}</li>`
+  )
+
+  return `
+    <div class="select__input" data-type="input-field">
+      <span>${inputText}</span>
+      <i class="fa fa-chevron-down" aria-hidden="true" data-type="arrow"></i>
+    </div>
+    <div class="select__dropdown">
+      <ul class="select__list">
+        ${selectItems.join(' ')}
+      </ul>
+    </div>
+  `
+}
 
 export class Select {
   constructor(selector, options) {
     this.domElem = document.querySelector(selector)
+    this.options = options
 
     this.#render()
     this.#setup()
   }
 
   #render() {
+    const { placeholder, data } = this.options
     this.domElem.classList.add('select')
-    this.domElem.innerHTML = getTemplate()
+    this.domElem.innerHTML = getTemplate(placeholder, data)
   }
 
   #setup() {
@@ -37,7 +42,12 @@ export class Select {
   clickHandler(e) {
     const { type } = e.target.dataset
 
-    type === 'input-field' && this.toggle()
+    if (type === 'input-field') {
+      this.toggle(type === 'input-field')
+    } else if (type === 'input-field') {
+      const id = e.target.dataset.value
+      console.log(id)
+    }
   }
 
   get isOpen() {
